f872280586c7ad2c03b18ee45e051b239663abb6
chaaerim <cofla411@ewhain.net>
2022-04-02 02:54:12 +0900

    chore : container height 단위 rem으로 통일

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const TodoContainer = styled.div`
   margin-top: 3rem;
   background: rgba(189, 187, 187, 0.8);
   width: 30rem;
-  height: 80vh;
+  height: 39rem;
   border-radius: 30px;
   box-shadow: 0 5px 18px -7px rgba(0, 0, 0, 1);
   padding: 30px;
